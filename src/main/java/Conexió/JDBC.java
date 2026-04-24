367e8d3ea725f0cb775b0ba346d5d170bf34f82f
ivian <i.viana@sapalomera.cat>
2026-04-24 12:28:21 +0200

    algo

--- src/main/java/Conexió/JDBC.java
+++ src/main/java/Conexió/JDBC.java
@@ -7,7 +7,14 @@ import java.sql.SQLException;
 //Dos tipos de conexiones necesarias(mysql y otra como postgres)
 //Menús
 //Conexion por operacion.
+//DAO
 
+//Sobre el readme:
+//Descripcion del proyecto
+//Como hemos planteado la estructura
+//La arquitectura de como lo hemos montado
+//Como hemos planteado las conexiones y el menú
+//Contras o problemas que se han tenido
 
 public class JDBC {
     private static final String URL = "jdbc:mysql://localhost:3306/pillam_db";
